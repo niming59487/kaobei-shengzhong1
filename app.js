e652955033526a997bab8646437d695282cca59c
niming59487 <aabb1010816@gmail.com>
2026-07-17 20:14:26 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+function sendPost(){
+
+    const message = document.getElementById("message").value;
+    const status = document.getElementById("status");
+
+
+    if(message.trim() === ""){
+        status.innerHTML = "請輸入內容";
+        return;
+    }
+
+
+    db.collection("posts").add({
+
+        content: message,
+
+        time: new Date()
+
+    })
+    .then(()=>{
+
+        status.innerHTML = "投稿成功！";
+
+        document.getElementById("message").value = "";
+
+    })
+    .catch((error)=>{
+
+        console.log(error);
+
+        status.innerHTML = "投稿失敗";
+
+    });
+
+}
