acfbda82bed37de9924b3b176fc367d110ae8a58
niming59487 <aabb1010816@gmail.com>
2026-07-17 20:12:54 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "你的API",
+  apiKey: "AIzaSyAcAZTR_3NOpFdePiuYtrT0Bach-U8Dnoc",
   authDomain: "kaobei-shenzhong.firebaseapp.com",
   projectId: "kaobei-shenzhong",
   storageBucket: "kaobei-shenzhong.firebasestorage.app",
   messagingSenderId: "26114861390",
-  appId: "1:26114861390:web:6a77ac8787b2e3036c961f"
+  appId: "1:26114861390:web:6a77ac8787b2e3036c961f",
+  measurementId: "G-M6H84Q5LVV"
 };
 
 firebase.initializeApp(firebaseConfig);
