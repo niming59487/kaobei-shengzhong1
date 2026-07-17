e2ca8ee94d083ea2845a8416b762c4cf74a84032
niming59487 <aabb1010816@gmail.com>
2026-07-17 20:08:47 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,12 @@
-// Firebase 設定
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAcAZTR_3NOpFdePiuYtrT0Bach-U8Dnoc",
+  apiKey: "你的API",
   authDomain: "kaobei-shenzhong.firebaseapp.com",
   projectId: "kaobei-shenzhong",
   storageBucket: "kaobei-shenzhong.firebasestorage.app",
   messagingSenderId: "26114861390",
-  appId: "1:26114861390:web:6a77ac8787b2e3036c961f",
-  measurementId: "G-M6H84Q5LVV"
+  appId: "1:26114861390:web:6a77ac8787b2e3036c961f"
 };
 
-
-// 初始化 Firebase
-
 firebase.initializeApp(firebaseConfig);
-
-
-// 連接 Firestore
 
 const db = firebase.firestore();
